823f3325507b8ad9186ac27069108cf1d52f39aa
fogaoquatrobocas <133917201+fogaoquatrobocas@users.noreply.github.com>
2024-09-25 08:32:34 -0300

    .

--- main.js
+++ main.js
@@ -24,10 +24,3 @@ let media = function(numero1,numero2,numero3){
 }
 
 console.log (media(10,2,3))
-
-function Divisao (numeroo1,numeroo2){
-    return (numeroo1/numeroo2);
-
-}
-
-console.log (Divisao(0,0))
