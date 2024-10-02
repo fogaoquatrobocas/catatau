8d57e4a968bf9b86aea27df50643fcaa258b7d00
fogaoquatrobocas <133917201+fogaoquatrobocas@users.noreply.github.com>
2024-10-02 07:40:53 -0300

    f

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
- function soma (num1,num2){
-    return num1+num2;
+//  function soma (num1,num2){
+//     return num1+num2;
     
     
-}
+// }
 
-console.log (soma(5,2));
-
-
+// console.log (soma(5,2));
 
 
-function AreaDoQuadrado (lado){
-    return lado*lado;
+
+
+// function AreaDoQuadrado (lado){
+//     return lado*lado;
     
-}
+// }
 
-console.log (AreaDoQuadrado(3));
-
-
-
-let media = function(numero1,numero2,numero3){
-    return (numero1+numero2+numero3)/3;
-
-}
-
-console.log (media(10,7,3))
+// console.log (AreaDoQuadrado(3));
 
 
-let nome =prompt('numero?');
-document.write(numero);
-parseInt(prompt('numero?'));
+
+// let media = function(numero1,numero2,numero3){
+//     return (numero1+numero2+numero3)/3;
+
+// }
+
+// console.log (media(10,7,3))
+
+
+//  let num1=parseInt(prompt('numero');
+//  let num2=parseInt(prompt('numero');
+//  let num3=parseInt(prompt('numero');
+
+// function media(x,y,z){ 
+// return(x+y+z)/3;
+// }
+//  document.write(media(num1,num2,num3));
+
+
+let estados=[PR,SC,RS,SP,RJ,PB,PE,PI,AC,AL,AP,AM,BA,CE,DF,ES,GO,MA,MT,MS,MG,PA,RN,RO,RR,SE,TO];
